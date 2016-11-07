5fae444bf81cf5966bc39d53e0f8af647111868a
Francis Carriere <senatorsfc@gmail.com>
2016-11-07 13:26:19 -0500

    Added verticalScroll flag

--- Example-Swift/build/Pods.build/Debug-iphoneos/MXPagerView.build/DerivedSources/MXPagerView_vers.c
+++ Example-Swift/build/Pods.build/Debug-iphoneos/MXPagerView.build/DerivedSources/MXPagerView_vers.c
@@ -1,1 +1,5 @@
- const unsigned char MXPagerViewVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MXPagerView  PROJECT:Pods-1" "\n"; const double MXPagerViewVersionNumber __attribute__ ((used)) = (double)1.;
+ extern const unsigned char MXPagerViewVersionString[];
+ extern const double MXPagerViewVersionNumber;
+
+ const unsigned char MXPagerViewVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MXPagerView  PROJECT:Pods-1" "\n";
+ const double MXPagerViewVersionNumber __attribute__ ((used)) = (double)1.;
--- Example-Swift/build/Pods.build/Debug-iphoneos/Pods-MXPagerView-Swift.build/DerivedSources/Pods_MXPagerView_Swift_vers.c
+++ Example-Swift/build/Pods.build/Debug-iphoneos/Pods-MXPagerView-Swift.build/DerivedSources/Pods_MXPagerView_Swift_vers.c
@@ -1,1 +1,5 @@
- const unsigned char Pods_MXPagerView_SwiftVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_MXPagerView_Swift  PROJECT:Pods-1" "\n"; const double Pods_MXPagerView_SwiftVersionNumber __attribute__ ((used)) = (double)1.;
+ extern const unsigned char Pods_MXPagerView_SwiftVersionString[];
+ extern const double Pods_MXPagerView_SwiftVersionNumber;
+
+ const unsigned char Pods_MXPagerView_SwiftVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_MXPagerView_Swift  PROJECT:Pods-1" "\n";
+ const double Pods_MXPagerView_SwiftVersionNumber __attribute__ ((used)) = (double)1.;
